3e8f34e1f766437fc69357e6eb49be9f097c9215
李成刚 <lichenggang@uniontech.com>
2023-11-18 23:50:20 +0800

    feat: update libavtp to 0.2.0

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+set -ev 
+
+mkdir build
+CFLAGS=-Wno-missing-braces meson . build
+ninja -C build/ \
+	test \
+	aaf-talker \
+	aaf-listener \
+	crf-talker \
+	crf-listener \
+	cvf-talker \
+	cvf-listener \
+	ieciidc-talker \
+	ieciidc-listener
